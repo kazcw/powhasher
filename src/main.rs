8f77b102798f3cb81e27e0817b13081a0bd68bbb
Kaz Wesley <kaz@lambdaverse.org>
2018-10-08 19:00:10 -0700

    eliminate WorkSource

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,24 @@
 // copyright 2017 Kaz Wesley
 
-// core_affinity (maintained)
-// hwloc (maintained)
-// serde+serde_json
-
-// libcpuid (rs wrapper broken, needs work/replacement)
-// memmap (no hugepage support)
-
 #![feature(alloc_system)]
-#![feature(chunks_exact)]
 
 // no allocs on hot paths anyway
 extern crate alloc_system;
 
-mod worksource;
-
 use std::fs::File;
-use std::sync::{Arc, Mutex};
 use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::{Arc, Mutex};
 use std::thread;
-use std::time::{Instant, Duration};
+use std::time::{Duration, Instant};
 
-use cn_stratum::client::{PoolClient, MessageHandler, RequestId, Job, JobAssignment, ErrorReply};
-use crate::worksource::WorkSource;
+use cn_stratum::client::{
+    ErrorReply, Job, JobAssignment, MessageHandler, PoolClient, PoolClientWriter, RequestId,
+};
 
+use byteorder::{ByteOrder, LE};
+use core_affinity::CoreId;
 use log::*;
 use serde_derive::{Deserialize, Serialize};
-use core_affinity::CoreId;
-use byteorder::{ByteOrder, LE};
 
 const AGENT: &str = "pow#er/0.2.0";
 
@@ -66,8 +57,7 @@ fn main() {
                 .value_name("FILE")
                 .help("Sets a custom config file")
                 .takes_value(true),
-        )
-        .get_matches();
+        ).get_matches();
 
     let cfg: Config = File::open(args.value_of("config").unwrap_or("./config.json"))
         .map(serde_json::from_reader)
@@ -75,39 +65,50 @@ fn main() {
         .unwrap();
     debug!("config: {:?}", &cfg);
 
-    let worksource = {
-        let cfg = &cfg.pool;
-        let keepalive = cfg.keepalive_s.map(Duration::from_secs);
-        let client = PoolClient::connect(&cfg.address, &cfg.login, &cfg.pass, keepalive, AGENT, Client::new).unwrap();
-        let work = client.handler().job_handle();
-        let writer = client.write_handle();
-        thread::Builder::new()
-            .name("poolclient".into())
-            .spawn(move || client.run())
-            .unwrap();
-        WorkSource::new(work, writer)
-    };
+    let client = PoolClient::connect(
+        &cfg.pool.address,
+        &cfg.pool.login,
+        &cfg.pool.pass,
+        cfg.pool.keepalive_s.map(Duration::from_secs),
+        AGENT,
+        Client::new,
+    ).unwrap();
+    let work = client.handler().job();
+    let job_id = client.handler().job_id();
+    let pool = client.write_handle();
+    thread::Builder::new()
+        .name("poolclient".into())
+        .spawn(move || client.run())
+        .unwrap();
 
     let core_ids = core_affinity::get_core_ids().unwrap();
     let worker_count = cfg.workers.len();
-    let workerstats: Vec<_> = cfg.workers
-        .into_iter()
-        .enumerate()
-        .map(|(i, w)| {
-            let hash_count = Arc::new(AtomicUsize::new(0));
-            let hash_counter = Arc::clone(&hash_count);
-            let ws = worksource.clone();
-            let core = core_ids[w.cpu as usize];
-            debug!("starting worker{} with config: {:?}", i, &w);
-            thread::Builder::new()
-                .name(format!("worker{}", i))
-                .spawn(move || run_worker(w, ws, hash_count, core, i as u32, worker_count as u32))
-                .unwrap();
-            hash_counter
-        })
-        .collect();
+    let mut workerstats = Vec::with_capacity(cfg.workers.len());
+    for (i, w) in cfg.workers.into_iter().enumerate() {
+        let hash_count = Arc::new(AtomicUsize::new(0));
+        workerstats.push(Arc::clone(&hash_count));
+        let core = core_ids[w.cpu as usize];
+        debug!("starting worker{} with config: {:?}", i, &w);
+        let worker = Worker {
+            cfg: w,
+            hash_count,
+            job_id: Arc::clone(&job_id),
+            work: Arc::clone(&work),
+            pool: Arc::clone(&pool),
+            core,
+            worker_id: i as u32,
+            step: worker_count as u32,
+        };
+        thread::Builder::new()
+            .name(format!("worker{}", i))
+            .spawn(move || worker.run())
+            .unwrap();
+    }
 
-    let mut prevstats: Vec<_> = workerstats.iter().map(|w| w.load(Ordering::Relaxed)).collect();
+    let mut prevstats: Vec<_> = workerstats
+        .iter()
+        .map(|w| w.load(Ordering::Relaxed))
+        .collect();
     let start = Instant::now();
     let mut prev_start = start;
     let mut total_hashes = 0;
@@ -126,8 +127,14 @@ fn main() {
             *prev = new;
         }
         total_hashes += cur_hashes;
-        println!("\ttotal (since last): {} H/s", (cur_hashes as f32) / dur_to_f32(&cur_dur));
-        println!("\ttotal (all time): {} H/s", (total_hashes as f32) / dur_to_f32(&total_dur));
+        println!(
+            "\ttotal (since last): {} H/s",
+            (cur_hashes as f32) / dur_to_f32(&cur_dur)
+        );
+        println!(
+            "\ttotal (all time): {} H/s",
+            (total_hashes as f32) / dur_to_f32(&total_dur)
+        );
         std::io::stdin().read_line(&mut String::new()).unwrap();
     }
 }
@@ -136,21 +143,24 @@ fn dur_to_f32(dur: &Duration) -> f32 {
     ((dur.as_secs() as f32) + (dur.subsec_nanos() as f32) / 1_000_000_000.0)
 }
 
-#[cfg(test)]
-mod tests {}
-
 pub struct Client {
     job: Arc<Mutex<Job>>,
+    job_id: Arc<AtomicUsize>,
 }
 
 impl Client {
     fn new(job: Job) -> Self {
         let job = Arc::new(Mutex::new(job));
-        Client { job }
+        let job_id = Arc::new(AtomicUsize::new(0));
+        Client { job, job_id }
     }
 
-    fn job_handle(&self) -> Arc<Mutex<Job>> {
+    fn job(&self) -> Arc<Mutex<Job>> {
         Arc::clone(&self.job)
+    }
+
+    fn job_id(&self) -> Arc<AtomicUsize> {
+        Arc::clone(&self.job_id)
     }
 }
 
@@ -158,6 +168,7 @@ impl MessageHandler for Client {
     fn job_command(&mut self, j: Job) {
         debug!("new job: {:?}", j);
         *self.job.lock().unwrap() = j;
+        self.job_id.fetch_add(1, Ordering::Relaxed);
     }
 
     fn error_reply(&mut self, _id: RequestId, error: ErrorReply) {
@@ -187,31 +198,39 @@ pub struct WorkerConfig {
     hasher: cryptonight::HasherConfig,
 }
 
-pub fn run_worker(cfg: WorkerConfig, mut worksource: WorkSource, hash_count: Arc<AtomicUsize>, core: CoreId, worker_id: u32, step: u32) -> ! {
-    // TODO: CoreId error handling
-    core_affinity::set_for_current(core);
-    let (mut target, blob, mut algo) = worksource.get_new_work().unwrap();
-    let start = (u32::from(blob[42]) << 24) + worker_id;
-    let nonce_seq = (start..).step_by(step as usize);
-    let mut hashes = cryptonight::hasher(&algo, &cfg.hasher, blob, nonce_seq.clone());
-    loop {
-        let mut nonces = nonce_seq.clone();
+struct Worker {
+    cfg: WorkerConfig,
+    hash_count: Arc<AtomicUsize>,
+    job_id: Arc<AtomicUsize>,
+    work: Arc<Mutex<Job>>,
+    pool: Arc<Mutex<PoolClientWriter>>,
+    core: CoreId,
+    worker_id: u32,
+    step: u32,
+}
+
+impl Worker {
+    fn run(self) -> ! {
+        core_affinity::set_for_current(self.core);
         loop {
-            let ws = &mut worksource;
-            let mut h = [0u8; 32];
-            h.copy_from_slice(&hashes.by_ref().next().unwrap());
-            let n = nonces.by_ref().next().unwrap();
-            if LE::read_u64(&h[24..]) <= target {
-                ws.submit(&algo, n, &h).unwrap();
-            }
-            hash_count.fetch_add(1, Ordering::Relaxed);
-            if let Some((newt, newb, newa)) = ws.get_new_work() {
-                hashes = cryptonight::hasher(&newa, &cfg.hasher, newb, nonce_seq.clone());
-                target = newt;
-                algo = newa;
-                break;
-            }
+            let jid = self.job_id.load(Ordering::Relaxed);
+            let job = self.work.lock().unwrap().clone();
+            let algo = job.algo().unwrap_or_else(|| "cn/1");
+            let start = (u32::from(job.blob()[42]) << 24) + self.worker_id;
+            let nonce_seq = (start..).step_by(self.step as usize);
+            let mut hashes =
+                cryptonight::hasher(algo, &self.cfg.hasher, job.blob().into(), nonce_seq.clone());
+            let mut nonces = nonce_seq.clone();
+            while {
+                let mut h = [0u8; 32];
+                h.copy_from_slice(&hashes.by_ref().next().unwrap());
+                let n = nonces.by_ref().next().unwrap();
+                if LE::read_u64(&h[24..]) <= job.target() {
+                    self.pool.lock().unwrap().submit(&job, n, &h).unwrap();
+                }
+                self.hash_count.fetch_add(1, Ordering::Relaxed);
+                self.job_id.load(Ordering::Relaxed) == jid
+            } {}
         }
     }
 }
-
